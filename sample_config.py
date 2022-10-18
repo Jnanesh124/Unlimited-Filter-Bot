bec3d3316ee65478cb782a7f3fc2f52036fa3b1a
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-18 21:13:04 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -48,3 +48,9 @@ class Config(object):
 
     # To record start time of bot
     BOT_START_TIME = time.time()
+    
+    # Auto Delete For Group Message (Self Delete) #
+    SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10))
+    SELF_DELETE = environ.get('SELF_DELETE', True)
+    if SELF_DELETE == "True":
+    SELF_DELETE = True
